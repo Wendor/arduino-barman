874ea190c4e0db1dee13a085ccd9ae9a85c7a3aa
Anton Solomonov <weny@bk.ru>
2020-08-03 15:14:57 +0700

    COM5 + Encoder

--- barman.ino
+++ barman.ino
@@ -1,7 +1,9 @@
 #include <lcdgfx.h>
+#include <GyverEncoder.h>
 #include "images/images.cpp"
 
 DisplaySSD1306_128x32_I2C display(-1);
+Encoder enc1(6, 5, 4);
 
 const int oledWidth = 128;
 const int oledHeight = 32;
@@ -9,21 +11,46 @@ const int totalSlots = 5;
 const int spriteSize = 16;
 const int slotSpacer = (oledWidth - spriteSize * totalSlots) / (totalSlots - 1);
 
+
+
+int slots[totalSlots] = {};
+int capacity = 0;
+int part = 0;
+int total = 0;
+
+unsigned long lastDisplayTime = 0;
+bool holded = false;
+
 void setup()
 {
+  enc1.setType(AUTO);
   display.setFixedFont( ssd1306xled_font6x8 );
   display.begin();
   display.clear();
+  Serial.begin(9600);
+
+  generateData();
 }
 
 void loop() {
-  for(int i = 0; i < totalSlots; i++) {
-    drawSlot(caps[random(3)], i);  
-  }
+  unsigned long currentTime = millis();
 
-  int part = 5 + random(10)*5;
-  int total = random(5,50)*part;
-  int capacity = random(100);
+  enc1.tick();
+  if (enc1.isRight()) part++;
+  if (enc1.isLeft()) part--;
+  if (enc1.isTurn()) lastDisplayTime = 0;
+  if (enc1.isHold() && !holded) {
+    generateData();
+  }
+  holded = enc1.isHold();
+
+  if(currentTime - lastDisplayTime < 2000) return;
+  lastDisplayTime = millis();
+  
+  for(int i = 0; i < totalSlots; i++) {
+    drawSlot(i);
+  }
+  
   String caption = "A " + String(part) + "ml/" + String(total) + "ml";
   int spaces = floor(oledWidth / 6) - 4 - caption.length() - String(capacity).length();
 
@@ -35,16 +62,24 @@ void loop() {
 
   drawCaption(caption);
   drawBattery(capacity);
-  delay(1000);
 }
 
-void drawSlot(const uint8_t bitmap[], int slot) {
+void generateData() {
+  for(int i = 0; i < totalSlots; i++) {
+    slots[i] = random(3);
+  }
+  capacity = random(100);
+  total = part*random(5,50);
+  lastDisplayTime = 0;
+}
+
+void drawSlot(int slot) {
   int x = slot*spriteSize + slot*slotSpacer;
-  display.drawBitmap1(x, oledHeight - spriteSize, spriteSize, spriteSize, bitmap);
+  display.drawBitmap1(x, oledHeight - spriteSize, spriteSize, spriteSize, caps[slots[slot]]);
 }
 
 void drawCaption(String text) {
-     display.printFixed(0, 0, text.c_str());
+  display.printFixed(0, 0, text.c_str());
 }
 
 void drawBattery(int capacity) {
